eda2d140a80b97246c5aa01a9461335979604a0e
236707 Szymon Zakrzewski <236707@edu.p.lodz.pl>
2022-04-23 00:08:28 +0200

    added making board and filing it with the data from txt file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        Board board1 = new Board();
-        board1.printBoard();
+    public static String INITIAL_FILE = "uklad_poczatkowy.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //LOAD INITIAL FILE
+        File file = new File(INITIAL_FILE);
+        Scanner read = new Scanner(new File(INITIAL_FILE));
+        int ROW = read.nextInt();
+        int COLUMN = read.nextInt();
+
+        //MAKE BOARD
+        int[][] gameBoard = new int[ROW][COLUMN];
+        for(int i = 0; i < ROW; i++){
+            for(int j = 0; j < COLUMN; j++){
+                gameBoard[i][j] = read.nextInt();
+            }
+        }
+        printBoard(gameBoard);
+
+    }
+
+    public static void printBoard(int[][] board) {
+        for(int i = 0; i < 4; i++){
+            System.out.print("|");
+            for(int j = 0; j < 4; j++){
+                System.out.print(board[i][j]);
+                if(board[i][j] < 10){
+                    System.out.print(" ");
+                }
+                System.out.print("|");
+            }
+            System.out.print("\n");
+        }
     }
 }
